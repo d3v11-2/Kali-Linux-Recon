c6c303d41120cdeed4d1006d2e3763c0c0cdb15b
Varun Sharma <varun.sharma0050@gmail.com>
2020-07-03 01:07:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ select choice in "${choices[@]}"; do
                 yes)
 
 					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+					wget  https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+					sudo tar -xvf go*.tar.gz
 					sudo mv $HOME/go /usr/local
 					export GOROOT=/usr/local/go
 					export GOPATH=$HOME/go
